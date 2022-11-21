5740facfdf7a9e89bebd764b899e557203421e3b
Brando Chiminelli <brando.chiminelli99@gmail.com>
2022-11-21 16:55:57 +0100

    add monitor titanic

--- src/huggingface-spaces-titanic-monitor/app.py
+++ src/huggingface-spaces-titanic-monitor/app.py
@@ -7,19 +7,19 @@ fs = project.get_feature_store()
 
 dataset_api = project.get_dataset_api()
 
-dataset_api.download("Resources/images/latest_iris.png")
-dataset_api.download("Resources/images/actual_iris.png")
+dataset_api.download("Resources/images/latest_passenger.png")
+dataset_api.download("Resources/images/actual_passenger.png")
 dataset_api.download("Resources/images/df_recent.png")
 dataset_api.download("Resources/images/confusion_matrix.png")
 
 with gr.Blocks() as demo:
     with gr.Row():
       with gr.Column():
-          gr.Label("Today's Predicted Image")
-          input_img = gr.Image("latest_iris.png", elem_id="predicted-img")
+          gr.Label("Today's Predicted Passenger Fate")
+          input_img = gr.Image("latest_passenger.png", elem_id="predicted-img")
       with gr.Column():          
-          gr.Label("Today's Actual Image")
-          input_img = gr.Image("actual_iris.png", elem_id="actual-img")        
+          gr.Label("Today's Actual Passenger Fate")
+          input_img = gr.Image("actual_passenger.png", elem_id="actual-img")        
     with gr.Row():
       with gr.Column():
           gr.Label("Recent Prediction History")
